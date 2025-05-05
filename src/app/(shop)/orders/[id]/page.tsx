9250ad3b8aba843d4ca6b26581e1a0e001c62e6e
Yahi-Dev <yahinnieltheking01@gmail.com>
2025-05-05 17:52:58 -0500

    Proyecto Final con NextJs

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderByIdPage() {
+  return (
+    <div>
+      <h1>Ver orden en particular</h1>
+    </div>
+  );
+}
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+       <h1>Hola mundo</h1>
+
+       <h1 className={titleFont.className}>Login page</h1>
+      </main>
+    </div>
+  );
+}
+
+
